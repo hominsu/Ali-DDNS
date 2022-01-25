af97da0f7f478ca27423cfa84c56bf54f1496ba5
HominSu <1774069959@qq.com>
2022-01-25 12:55:31 +0800

    👷 Add jwt token gen and parse

---
+++ pkg/jwt.go
@@ -0,0 +1,52 @@
+package pkg
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"time"
+)
+
+var jwtKey = []byte("ddns.haomingsu.cn")
+
+type Claims struct {
+	Username string
+	jwt.RegisteredClaims
+}
+
+type User struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func GenToken(username string) (string, error) {
+	expireTime := jwt.NewNumericDate(time.Now().Add(time.Hour))
+	claims := &Claims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "127.0.0.1",
+			Subject:   "user token",
+			ExpiresAt: expireTime,
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ParseToken(tokenString string) (*jwt.Token, *Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(*jwt.Token) (interface{}, error) {
+		return []byte("ddns.haomingsu.cn"), nil
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return token, claims, nil
+}
